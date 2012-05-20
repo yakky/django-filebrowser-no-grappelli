ffb78a975b5c0fc24f8eaa0efa4a07d6e26e9aed
smacker <max@smacker.ru>
2012-05-20 18:18:47 +0600

    remove grapilli from setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 setup(
     name='django-filebrowser',
     version='3.4.3',
-    description='Media-Management with Grappelli',
+    description='Media-Management',
     long_description = read('README.rst'),
     author='Patrick Kranzlmueller, Axel Swoboda, Vaclav Mikolasek (vonautomatisch)',
     author_email='office@vonautomatisch.at',
@@ -26,7 +26,4 @@ setup(
         'Programming Language :: Python',
     ],
     zip_safe = False,
-    install_requires = [
-        'django-grappelli >= 2.3.7',
-    ],
 )
